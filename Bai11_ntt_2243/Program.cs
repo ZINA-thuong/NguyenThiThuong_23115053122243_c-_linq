dfbd17dffd20c6689483b613079a359eea37a181
Nguyen Thi Thuong <namthuong219@gmail.com>
2026-03-20 10:42:07 +0700

    Sắp xếp chúng giảm dần rồi lấy phần tử đầu tiên

--- Bai11_ntt_2243/Program.cs
+++ Bai11_ntt_2243/Program.cs
@@ -17,7 +17,7 @@ namespace Bai11_ntt_2243
         static void Main(string[] args)
         {//
             Console.WriteLine("Nguyen Thi Thuong _23115053122243_LTC#02");
-
+            //
             List<Student> danhSachSV = new List<Student>() {
                 new Student { Id = 1, Name = "An", Score = 8 },
                 new Student { Id = 2, Name = "Binh", Score = 6 },
