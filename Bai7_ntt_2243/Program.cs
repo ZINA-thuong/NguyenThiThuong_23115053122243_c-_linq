14a72d68526434d4f2a9b74ac00b95e8e07b6d23
Nguyen Thi Thuong <namthuong219@gmail.com>
2026-03-20 11:50:50 +0700

    Add project files.

---
+++ Bai7_ntt_2243/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bai7_ntt_2243
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Nguyen Thi Thuong _ 23115053122243 _ 225LTC#02");
+            List<int> numbers = new List<int> { 1, 52, 3, 4, 9, 6, 7, 78, 8 };
+            Console.WriteLine("Danh sach ban dau: " + string.Join(", ", numbers));
+
+            bool soLonHon10 = numbers.Any(n => n > 10);
+            // kết quả là 1 số ko phải list danh sách  => ko dùng list , toList
+            if (soLonHon10)
+            {
+                Console.WriteLine("Co so > 10!");
+            }else
+            {
+                Console.WriteLine("Khong Co so > 10!");
+            }
+            Console.ReadLine();
+        }
+    }
+}
