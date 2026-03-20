2cc8f3b6c047766acdb42f17184802a5d3d1dd07
Nguyen Thi Thuong <namthuong219@gmail.com>
2026-03-20 15:14:06 +0700

    Add project files.

---
+++ Bai20/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bai20
+{
+    class Program
+    {
+        class Student
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+            public double Score { get; set; }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Nguyen Thi Thuong _23115053122243_LTC#02");
+
+            List<Student> danhSachSV = new List<Student>() {
+                new Student { Id = 1, Name = "An", Score = 8 },
+                new Student { Id = 2, Name = "Binh", Score = 6 },
+                new Student { Id = 3, Name = "Chi", Score = 9 },
+                new Student { Id = 4, Name = "Dung", Score = 4 }
+            };
+            Console.WriteLine("Danh sach sv ban dau");
+            foreach (Student sv in danhSachSV)
+                Console.WriteLine(sv.Name + "-" + sv.Score);
+
+            List<Student> top3 = danhSachSV.OrderByDescending(s => s.Score).Take(3).ToList();
+
+            Console.WriteLine("\nBài 20 - Top 3 sinh viên xuất sắc:");
+            foreach (var sv in top3)
+            {
+                Console.WriteLine($"{sv.Name} - {sv.Score} điểm");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
