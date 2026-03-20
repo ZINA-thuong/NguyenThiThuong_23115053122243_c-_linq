9c672be5da2069f3cd38540dacc617d9ab1208bb
Nguyen Thi Thuong <namthuong219@gmail.com>
2026-03-20 09:16:34 +0700

    Dung count

--- Bai6_ntt_2243/Program.cs
+++ Bai6_ntt_2243/Program.cs
@@ -12,7 +12,7 @@ namespace Bai6_ntt_2243
         {
             Console.WriteLine("Nguyen Thi Thuong _ 23115053122243 _ 225LTC#02");
             List<int> numbers = new List<int> { 1, 52, 3, 4, 9, 6, 7, 78, 8 };
-            Console.WriteLine("Danh sach ban dau: " + string.Join(", ", numbers));//
+            Console.WriteLine("Danh sach ban dau: " + string.Join(", ", numbers));
 
             var soChan = numbers.Count(n => n % 2 == 0); 
             // kết quả là 1 số ko phải list danh sách  => ko dùng list , toList
