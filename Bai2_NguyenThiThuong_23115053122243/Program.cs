9cb28415fe4bb78d6bc7fd89437d87d3464408db
Nguyen Thi Thuong <namthuong219@gmail.com>
2026-03-20 08:49:50 +0700

    Dùng list để lấy ds số > 5 => in ra nối chuối

--- Bai2_NguyenThiThuong_23115053122243/Program.cs
+++ Bai2_NguyenThiThuong_23115053122243/Program.cs
@@ -13,7 +13,7 @@ namespace Bai2_NguyenThiThuong_23115053122243
             Console.WriteLine("Nguyen Thi Thuong _ 23115053122243 _ 225LTC#02");
             List<int> numbers = new List<int> { 1, 2, 3, 4, 5, 6, 7, 78, 8 };
 
-            List<int> SoLon5 = numbers.Where(n => n > 5).ToList();
+            List<int> SoLon5 = numbers.Where(n => n > 5).ToList();//
             Console.WriteLine("Danh sach so > 5: " + string.Join(", ", SoLon5));
             Console.ReadLine();
         }
