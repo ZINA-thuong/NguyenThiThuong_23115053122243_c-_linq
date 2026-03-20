afd3361a8c08b5753da0eae696d87f84cec3ca85
Nguyen Thi Thuong <namthuong219@gmail.com>
2026-03-20 09:08:13 +0700

    Add project files.

---
+++ Bai5_ntt_2243/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bai5_ntt_2243
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Nguyen Thi Thuong _ 23115053122243 _ 225LTC#02");
+            List<int> numbers = new List<int> { 1, 52, 3, 4, 9, 6, 7, 78, 8 };
+            Console.WriteLine("Danh sach ban dau: " + string.Join(", ", numbers));
+
+            List<int> binhPhuong = numbers.Select(n => n*n).ToList();
+            Console.WriteLine("Danh sach binh phuong: " + string.Join(", ", binhPhuong));
+            Console.ReadLine();
+        }
+    }
+}
