b78f0a3d80948ee19d2de931cb7de560559433a8
Nguyen Thi Thuong <namthuong219@gmail.com>
2026-03-20 09:09:09 +0700

    Dùng Select()

--- Bai5_ntt_2243/Program.cs
+++ Bai5_ntt_2243/Program.cs
@@ -12,7 +12,7 @@ namespace Bai5_ntt_2243
         {
             Console.WriteLine("Nguyen Thi Thuong _ 23115053122243 _ 225LTC#02");
             List<int> numbers = new List<int> { 1, 52, 3, 4, 9, 6, 7, 78, 8 };
-            Console.WriteLine("Danh sach ban dau: " + string.Join(", ", numbers));
+            Console.WriteLine("Danh sach ban dau: " + string.Join(", ", numbers));//
 
             List<int> binhPhuong = numbers.Select(n => n*n).ToList();
             Console.WriteLine("Danh sach binh phuong: " + string.Join(", ", binhPhuong));
