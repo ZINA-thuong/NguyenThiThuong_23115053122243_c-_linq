c849caa17581434575508256c6cc5ff4e1e4751e
Nguyen Thi Thuong <namthuong219@gmail.com>
2026-03-20 14:46:15 +0700

    gom nhóm => sắp xếp giãm => lấy số đầu tiên

--- Bai19_ntt_2243/Program.cs
+++ Bai19_ntt_2243/Program.cs
@@ -10,6 +10,15 @@ namespace Bai19_ntt_2243
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Nguyen Thi Thuong _ 23115053122243 _ 225LTC#02");
+            List<int> numbers = new List<int> { 1, 2, 2, 3, 4, 4, 5 };
+            Console.WriteLine("Danh sach ban dau: " + string.Join(", ", numbers));
+
+            // PT nào xuất hiện nhiều nhất 
+            var PTnhieunhat = numbers.GroupBy(n => n).OrderByDescending(g=>g.Count()).FirstOrDefault();
+            Console.WriteLine($"Phan tu nhieu nhat là {PTnhieunhat.Key}: {PTnhieunhat.Count()} lan");
+            Console.ReadLine();
         }
     }
 }
+
