a580bd72614355fface81fe2bc752289184570d2
Nguyen Thi Thuong <namthuong219@gmail.com>
2026-03-20 12:36:41 +0700

    Add project files.

---
+++ Bai1_NguyenThiThuong_2243/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bai1_NguyenThiThuong_2243
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8 };
+
+            var soChan = numbers.Where(n => n % 2 == 0).ToList();
+            Console.Write("Danh sach so chan: ");
+            foreach (var n in soChan)
+                Console.Write($"{n} ");
+            Console.ReadLine();
+        }
+    }
+}
