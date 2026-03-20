4071a74a1fcbbfdbfe99b4f116e2b0f3920a2402
Nguyen Thi Thuong <namthuong219@gmail.com>
2026-03-20 10:48:35 +0700

    Add project files.

---
+++ Bai12_ntt_2243/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bai12_ntt_2243
+{
+    class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Score { get; set; }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {//
+            Console.WriteLine("Nguyen Thi Thuong _23115053122243_LTC#02");
+            //
+            List<Student> danhSachSV = new List<Student>() {
+                new Student { Id = 1, Name = "An", Score = 8 },
+                new Student { Id = 2, Name = "Binh", Score = 6 },
+                new Student { Id = 3, Name = "Chi", Score = 9 },
+                new Student { Id = 4, Name = "Dung", Score = 7 }
+            };
+            Console.WriteLine("Danh sach sv ban dau");
+            foreach (Student sv in danhSachSV)
+                Console.WriteLine(sv.Name + "-" + sv.Score);
+
+            //đếm sv >= 7 điểm
+            var SVLon7 = danhSachSV.Count(s => s.Score >= 7);
+            Console.WriteLine($"Co {SVLon7} lon hon hoac bang 7 diem ");
+
+            Console.ReadLine();
+        }
+    }
+}
