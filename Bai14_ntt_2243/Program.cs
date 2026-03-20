806b036e63cb0f3266213518261f91501f030964
Nguyen Thi Thuong <namthuong219@gmail.com>
2026-03-20 11:07:49 +0700

    Dùng any để kiểm tra

--- Bai14_ntt_2243/Program.cs
+++ Bai14_ntt_2243/Program.cs
@@ -19,7 +19,7 @@ namespace Bai14_ntt_2243
             Console.WriteLine("Nguyen Thi Thuong _23115053122243_LTC#02");
             //
             List<Student> danhSachSV = new List<Student>() {
-                new Student { Id = 1, Name = "An", Score = 8 },
+                new Student { Id = 1, Name = "An", Score = 8 },//
                 new Student { Id = 2, Name = "Binh", Score = 6 },
                 new Student { Id = 3, Name = "Chi", Score = 9 },
                 new Student { Id = 4, Name = "Dung", Score = 7 }
