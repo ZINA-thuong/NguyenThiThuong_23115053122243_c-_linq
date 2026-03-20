8012d39c3eb284efcc034c8b14b53b27c086f84d
Nguyen Thi Thuong <namthuong219@gmail.com>
2026-03-20 13:51:19 +0700

    Add project files.

---
+++ Bai19_ntt_2243/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bai19_ntt_2243
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
